c7dc98e9fb76e427cd613132f927189567b00dc6
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2026-05-24 21:55:46 +0530

    logo and fonts modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import DashboardFab from './components/common/DashboardFab/DashboardFab';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <DashboardFab />
     </BrowserRouter>
   );
 }
